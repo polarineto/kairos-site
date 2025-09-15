c47ef67c0458d036a715e5c6c3e80c3f5a307c0e
polarineto <polarineto@gmail.com>
2025-09-15 17:52:15 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ export const metadata = {
   description: 'Conselheiros estratégicos para empresas no mercado financeiro.',
 }
 
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
