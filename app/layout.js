78b688db2cd24994457b1bfae9f434386831164c
polarineto <polarineto@gmail.com>
2025-09-16 14:13:52 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,14 @@
+// app/layout.js
+
 export const metadata = {
   title: 'Kairos Inteli.tech',
   description: 'Conselheiros estratégicos para empresas no mercado financeiro.',
 };
 
-import './globals.css';
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
-      <body className="font-modern text-gray-800">
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body className="bg-white text-gray-900">{children}</body>
     </html>
   );
 }
