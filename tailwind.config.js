8925eac0c7b46567f5cc0f354a0f4c5da467c142
polarineto <polarineto@gmail.com>
2025-09-16 12:31:04 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        olive: "#1b4820",
+        "olive-dark": "#143819",
+        gold: "#bfa144",
+        "gold-light": "#f4e2b5",
+        "gold-dark": "#8a6c1e",
+      },
+    },
   },
   plugins: [],
-}
+};
