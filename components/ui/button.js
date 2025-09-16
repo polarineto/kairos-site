c7b6fc1d7ffb75092b2ffe87fce6b3c919624d50
polarineto <polarineto@gmail.com>
2025-09-16 12:24:56 -0300

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,4 +1,12 @@
-"use client"
-export function Button({ children, ...props }) {
-  return <button {...props} className="px-4 py-2 rounded font-semibold">{children}</button>;
+"use client";
+
+export function Button({ children, className = "", ...props }) {
+  return (
+    <button
+      {...props}
+      className={`px-4 py-2 rounded font-semibold transition-colors duration-300 ${className}`}
+    >
+      {children}
+    </button>
+  );
 }
