0f340fc904ec9034a874b4654d80616d9745e591
polarineto <polarineto@gmail.com>
2025-09-16 12:27:12 -0300

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,4 +1,10 @@
-"use client"
-export function Input(props) {
-  return <input {...props} className="border rounded p-2" />;
+"use client";
+
+export function Input({ className = "", ...props }) {
+  return (
+    <input
+      {...props}
+      className={`w-full border border-gray-300 rounded-lg px-4 py-2 text-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gold focus:border-transparent ${className}`}
+    />
+  );
 }
