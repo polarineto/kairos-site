e84ec0f99bd55ed6750356491df9968804b7de70
polarineto <polarineto@gmail.com>
2025-09-16 14:02:51 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,15 +10,6 @@ export default function RootLayout({ children }) {
     <html lang="pt-br">
       <body className="font-modern text-gray-800">
         {children}
-
-        {/* FOOTER */}
-        <footer className="bg-olive-dark text-white py-8 mt-12">
-          <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
-            <p className="text-sm text-gray-200">
-              &copy; {new Date().getFullYear()} Kairós Inteli.tech. Todos os direitos reservados.
-            </p>
-          </div>
-        </footer>
       </body>
     </html>
   );
