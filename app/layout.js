e5d618a911b483a30a35f421a7772fd5f297e56e
polarineto <polarineto@gmail.com>
2025-09-16 14:00:44 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ export const metadata = {
 };
 
 import './globals.css';
-import Header from '../components/Header';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className="font-modern text-gray-800">
-        <Header />
         {children}
+
+        {/* FOOTER */}
         <footer className="bg-olive-dark text-white py-8 mt-12">
           <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
             <p className="text-sm text-gray-200">
